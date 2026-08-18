0e21f8959d1bfb1822c303877314cd16e6556b43
konyonuku <295836839+konyonuku@users.noreply.github.com>
2026-08-18 18:53:42 +0900

    feat view/projection matrix

--- Engine/Math/Matrix.h
+++ Engine/Math/Matrix.h
@@ -68,6 +68,31 @@ struct Matrix4x4 {
         res.m[3][0] = x, res.m[3][1] = y, res.m[3][2] = z;
         return res;
     }
+
+    static Matrix4x4 LookAtLH(const Vector3& eye, const Vector3& target, const Vector3& up) {
+        const Vector3 zAxis = (target-eye).Normalized();
+        const Vector3 xAxis = up.Cross(zAxis).Normalized();
+        const Vector3 yAxis = zAxis.Cross(xAxis);
+
+        Matrix4x4 res = Identity();
+        res.m[0][0] = xAxis.x,          res.m[0][1] = yAxis.x,          res.m[0][2] = zAxis.x;
+        res.m[1][0] = xAxis.y,          res.m[1][1] = yAxis.y,          res.m[1][2] = zAxis.y;
+        res.m[2][0] = xAxis.z,          res.m[2][1] = yAxis.z,          res.m[2][2] = zAxis.z;
+        res.m[3][0] = -xAxis.Dot(eye),  res.m[3][1] = -yAxis.Dot(eye),  res.m[3][2] = -zAxis.Dot(eye);
+        return res;        
+    }
+
+    static Matrix4x4 PerspectiveFovLH(float fovY, float aspect, float nearZ, float farZ) {
+        Matrix4x4 res {};
+        const float t = std::tan(fovY * 0.5f);
+
+        res.m[0][0] = 1.f / (aspect * t);
+        res.m[1][1] = 1.f / t;
+        res.m[2][2] = farZ / (farZ - nearZ);
+        res.m[2][3] = 1.f;
+        res.m[3][2] = -nearZ * farZ / (farZ - nearZ);
+        return res;
+    }
 };
 
 inline Vector4 operator*(const Vector4 v, const Matrix4x4& m) {
